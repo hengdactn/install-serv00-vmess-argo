cb64427bc03dec55318cd3c75c0d9a2734102a03
飞翔 <70909689+hengdactn@users.noreply.github.com>
2024-09-01 20:23:19 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 echo "auth=$SERV00_S9_TUNNEL_API"
-echo "test=:$UUID"
+echo "uuid=:$UUID"
 echo "Secret value: $SERV00_S9_TUNNEL_API"
 if [ -z "$SERV00_S9_TUNNEL_API" ]; then
     echo "SERV00_S9_TUNNEL_API is not set or is empty"
@@ -7,7 +7,7 @@ else
     echo "Secret value: $SERV00_S9_TUNNEL_API"
 fi
 if [ -z "$TEST" ]; then
-    echo "test is not set or is empty"
+    echo "uuid is not set or is empty"
 else
     echo "Secret value: $TEST"
 fi
