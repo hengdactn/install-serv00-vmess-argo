9f8a63821268ca8161af977cfb0835778b9f33bb
飞翔 <70909689+hengdactn@users.noreply.github.com>
2024-09-01 18:19:58 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
 echo "auth=$SERV00_S9_TUNNEL_API"
 echo "test=:$TEST"
 echo "Secret value: $SERV00_S9_TUNNEL_API"
+if [ -z "$SERV00_S9_TUNNEL_API" ]; then
+    echo "SERV00_S9_TUNNEL_API is not set or is empty"
+else
+    echo "Secret value: $SERV00_S9_TUNNEL_API"
+fi
